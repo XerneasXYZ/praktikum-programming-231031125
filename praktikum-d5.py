4f4b9b5940bda26e02342340ab186c01f34d5cb0
adam <130664719+XerneasXYZ@users.noreply.github.com>
2023-12-17 00:09:52 -0800

    upload file 5

---
+++ praktikum-d5.py
@@ -0,0 +1,24 @@
+a = True
+i = 0
+limit = 5
+
+while a:
+    i+=1 #i = i + 1
+    if i <= limit:
+        print ("Selamat Bergabung",limit+1-i)
+    else:
+        print('program berhenti disini !!!!!!') 
+        a = False
+
+
+
+
+
+
+
+
+
+
+
+
+
