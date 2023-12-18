f798ad9b9d56f7de6184368a456b9e59141ed746
adam <130664719+XerneasXYZ@users.noreply.github.com>
2023-12-17 16:19:40 -0800

    upload file 9

---
+++ adamsyahqwan-231031125-dictionaries.py
@@ -0,0 +1,15 @@
+mhs = {'nama'  : 'Adam Syahqwan', 
+       'nim'   : '231031125',
+       'lulus' : True,
+       'asal'  : 'Indonesia',
+       'sd'     : 'SDN2',
+       'smp'  : 'SMP4',
+       'sma' :  'SMK5',
+       'matkul': {'mk1' : 'Agama',
+                  'mk2' : 'bahasa',
+                  'mk3' : 'pemrograman'
+                 }
+       }
+print(mhs['nama'])
+print(mhs['asal'])
+print(mhs['matkul']['mk2'])
