b8cd6696966024808cf3447a35e82ba35abcb25c
adam <130664719+XerneasXYZ@users.noreply.github.com>
2023-12-17 18:55:06 -0800

    upload file 14

---
+++ 231031125-adamsyahqwan-percabangan3.py
@@ -0,0 +1,11 @@
+nama = input("masukkan nama : ")
+pendapatan = int(input("masukkan besaran pendapatan : "))
+
+print ('nama karyawan : ', nama)
+print ('pendapatan : ', pendapatan)
+
+if pendapatan > 1000:
+    print ('status : berhakaat')
+else:
+    print ('status : tidak berhak')
+
